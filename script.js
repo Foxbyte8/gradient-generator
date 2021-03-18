3b90dc92186796e82f2201d1082792441d7cdd08
Malachi Miller <mjxyz8@gmail.com>
2021-03-18 16:38:34 -0500

    adding starting project

---
+++ script.js
@@ -0,0 +1,13 @@
+const code = document.querySelector("#code");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const body = document.querySelector("#gradient");
+
+function setGradient() {
+  const gradientStyle = `linear-gradient(to right, ${color1.value}, ${color2.value});`;
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  code.textContent = gradientStyle;
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
