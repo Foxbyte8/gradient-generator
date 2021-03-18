10de78e18bfa06bb283846421b7eff69ec66328c
Malachi Miller <mjxyz8@gmail.com>
2021-03-18 16:56:01 -0500

    Added comments to the code

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// Variables
 const code = document.querySelector("#code");
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const body = document.querySelector("#gradient");
 
+
+// set the gradient to the background
 function setGradient() {
   const gradientStyle = `linear-gradient(to right, ${color1.value}, ${color2.value});`;
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   code.textContent = gradientStyle;
 }
 
+// input
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
