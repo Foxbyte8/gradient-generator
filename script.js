ef0890b15e0186f63bd09e98534e2a85fd8522f8
Malachi Miller <mjxyz8@gmail.com>
2021-03-18 16:57:08 -0500

    Removed some white space

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const body = document.querySelector("#gradient");
 
-
 // set the gradient to the background
 function setGradient() {
   const gradientStyle = `linear-gradient(to right, ${color1.value}, ${color2.value});`;
